8efa1499c832a991a2c45d359eeb146626dace15
edwardforex <edwardforex3@gmail.com>
2023-02-20 13:29:43 -0800

    4-puts.c testfile

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is a program that prints a line with the help of s put function
+ * return (0) (sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
