a8b22e4872cd17b068297768f74cdcc0459a22f0
edwardforex <edwardforex3@gmail.com>
2023-02-23 16:40:45 -0800

    5-printf.c testfile

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this program uses printf function
+ *
+ * description: main
+ *
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0)
+}
